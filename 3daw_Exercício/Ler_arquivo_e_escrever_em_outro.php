62c95cfb9a6c2614427461b555757cba0f0c5308
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-05-18 23:20:27 -0300

    Update Ler_arquivo_e_escrever_em_outro.php

--- 3daw_Exercício/Ler_arquivo_e_escrever_em_outro.php
+++ 3daw_Exercício/Ler_arquivo_e_escrever_em_outro.php
@@ -20,32 +20,32 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $nome = $_POST["nome"];
     $emai = $_POST["email"];
     $data = $_POST["dataNasc"];
-    $arquivoAluno = fopen("Aluno.txt", "r") or die ("Arquivo com problema");
-    echo "imprimir conteudo: " .fgets($arquivoAluno);
-    fclose($arquivoAluno);
-    $arquivoAluno2 = fopen("Aluno.txt","w") or die ("Arquivo com problema");
-    $header ="matricula;nome;email;dataNasc\n";
-    fwrite($arquivoAluno2,$header);
-        $linha = $matricula . ";" . $nome . ";" . $emai. ";" . $data . "\n";
-    fwrite($arquivoAluno2, $linha);
-    fclose($arquivoAluno2);
-    $arquivoAlunoIn = fopen("Aluno.txt","r") or die ("Arquivo com problema");
-    while(!feof($arquivoAlunoIn)){
-        $linhas [] = fgets($arquivoAlunoIn);
+    $linha_final = $matricula . ";" . $nome . ";" . $emai. ";" . $data . "\n";
+    $arquivo = fopen('Aluno.txt','r+');
+    $arquivo2 = fopen('Aluno2.txt', 'w');
+    if ($arquivo){
+    while(true){ 
+        $linha = fgets($arquivo);
+        if ($linha==null) break;
+        if(preg_match("/$matricula/", $linha)){
+        $linha_inicio=$linha;
+        $string = str_replace($linha_inicio, $linha_final, $linha);
+        } else {
+        $string = $linha;
+        }
+        fwrite($arquivo2, $string);
+        }
+    fclose($arquivo);
+    fclose($arquivo2);
     }
-    $arquivoAluno = fopen("Aluno2.txt", "w");
-    fwrite($arquivoAluno, $header);
-    fwrite($arquivoAluno, $linha);
-    fclose($arquivoAluno);
 }    
 ?>
-<form action= "incluir_aluno.php" method = POST>
-    Matrícula:<input type= text name= "matricula" value=''><br>
+<form action= "Editar.php" method = POST>
+    Digite a Matrícula que deseja alterar:<input type= text name= "matricula" value=''><br>
     Nome:<input type= text name= "nome" value=''><br>
     email:<input type= text name= "email" value=''><br>
     Data Nascimento:<input type= text name= "dataNasc" value=''><br>
-    <input type="submit" value="enviar">
+    <input type="submit" value="alterar">
 </form>
-
 </body>
 </html>
