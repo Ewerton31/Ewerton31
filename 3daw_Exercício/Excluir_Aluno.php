0a1aaffb7c0d34350b6c885805bc623e7f185714
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-05-18 23:50:09 -0300

    Add files via upload

---
+++ 3daw_Exercício/Excluir_Aluno.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+    body{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) 
+    }
+    </style>
+</head>
+
+<body>
+<h1>Excluir Aluno</h1>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $matricula = $_POST["matricula"];
+    $arquivo = fopen('Aluno.txt','r+');
+    $arquivo2 = fopen('Aluno2.txt', 'w');
+    if ($arquivo){
+    while(true){ 
+        $linha = fgets($arquivo);
+        if ($linha==null) break;
+        if(preg_match("/$matricula/", $linha)){
+            echo 'Arquivo Excluido com sucesso/n';
+        } else {
+        $string = $linha;
+        fwrite($arquivo2, $string);
+        }
+        }
+    fclose($arquivo);
+    fclose($arquivo2);
+    }
+    $arquivo2 = fopen('Aluno2.txt','r+');
+    $arquivo = fopen('Aluno.txt', 'w');
+    if ($arquivo){
+        while(true){ 
+            $linha = fgets($arquivo2);
+            if ($linha==null) break;
+            $string = $linha;
+            fwrite($arquivo, $string);
+        }
+        fclose($arquivo);
+        fclose($arquivo2);
+    }
+}    
+?>
+<form action= "Excluir_Aluno.php" method = POST>
+    Digite a Matrícula que deseja alterar:<input type= text name= "matricula" value=''><br>
+    <input type="submit" value="Excluir">
+</form>
+</body>
+</html>
