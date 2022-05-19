9133f6a4c751f2c189ad458a0ec2f9541978500c
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-05-19 03:50:32 -0300

    Update Listar_todos.php

--- Exercício_CRUD_de_Produtos/Listar_todos.php
+++ Exercício_CRUD_de_Produtos/Listar_todos.php
@@ -14,12 +14,26 @@
 
 <body>
 <h1>Listar todos</h1>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $arquivo = fopen('Produto.txt','r+');
+    if ($arquivo){
+    while(true){ 
+        $linha = fgets($arquivo);
+        if ($linha==null) break;
+        $string = $linha;
+        echo nl2br("$string\n");
+    }
+    fclose($arquivo);
+    }
+}    
+?>
 <table>
     <tr>
         <form action= "Incluir_produto.php" method = POST>
             <input type="submit" value="Incluir">
         </form>
-        <form action= "Alterar_produto.html" method = POST>
+        <form action= "Alterar_produto.php" method = POST>
             <input type="submit" value="Alterar">
         </form>
         <form action= "Listar_todos.php" method = POST>
@@ -33,5 +47,8 @@
         </form>
     </tr>
 </table>
+<form action= "Listar_todos.php" method = GET>
+    <input type="submit" value="Listar Todos">
+</form>
 </body>
 </html>
