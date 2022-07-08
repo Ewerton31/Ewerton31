92d53a3509f9694c0cc8f80ed951b26ec3b098fa
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-08 13:58:35 -0300

    Update Lista_todos_Onibus.php

--- Av2.2022_Ewerton_Victor/Lista_todos_Onibus.php
+++ Av2.2022_Ewerton_Victor/Lista_todos_Onibus.php
@@ -1,47 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style type="text/css">
-    body{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%) 
-    }
-    </style>
-</head>
-
-<body>
-<h1>Listar todos os Onibus</h1>
-<table>
-    <tr>
-        <form action= "Criar_Onibus.php" method = POST>
-            <input type="submit" value="Criar">
-        </form>
-        <form action= "Alterar_Onibus.html" method = POST>
-            <input type="submit" value="Alterar">
-        </form>
-        <form action= "Lista_todos_Onibus.php" method = POST>
-            <input type="submit" value="Listar Todos">
-        </form>
-        <form action= "Lista_um_Onibus.php" method = POST>
-            <input type="submit" value="Listar um">
-        </form>
-        <form action= "Excluir_Onibus.php" method = POST>
-            <input type="submit" value="Excluir">
-        </form>
-    </tr>
-</table>
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if($_SERVER["REQUEST_METHOD"]=="GET"){
     include_once("conexao.php");
     $sql = "SELECT * FROM `onibus`";
     $resultado = mysqli_query($conn, $sql);
+    $Lista="";
     while($ler_resultado = mysqli_fetch_assoc($resultado)){
-        echo "Id:" . $ler_resultado['Id'] . "&ensp; Marca:" . $ler_resultado['Marca'] ."&emsp;Modelo:" . $ler_resultado['Modelo'] ."&emsp;qtdAssentos:" . $ler_resultado['qtdAssentos'] ."&emsp;temBanheiro:" . $ler_resultado['temBanheiro']."&emsp;temArCondicionado:" . $ler_resultado['temArCondicionado']."&emsp;Chassis:" . $ler_resultado['Chassis']."&emsp;Placa:" . $ler_resultado['Placa'] ."<br><hr>";
+        //echo "<td>". $ler_resultado['Id'] . $ler_resultado['Marca'] . $ler_resultado['Modelo'] . $ler_resultado['qtdAssentos'] . $ler_resultado['temBanheiro'] . $ler_resultado['temArCondicionado'] . $ler_resultado['Chassis'] . $ler_resultado['Placa'] ."</td><br>";
+        extract($ler_resultado);
+        $Lista .= "<table><tr><td>$Id</td><td>$Marca</td><td>$Modelo</td><td>$qtdAssentos</td><td>$temBanheiro</td><td>$temArCondicionado</td><td>$Chassis</td><td>$Placa</td></tr> </table>";
+        
+        //var_dump($ler_resultado);
+        //echo  "<td>".$Id."</td>";
+        /*echo  "<td>".$ler_resultado['Marca']."</td>";
+        echo  "<td>".$ler_resultado['Modelo']."</td>";
+        echo  "<td>".$ler_resultado['qtdAssentos']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";
+        echo  "<td>".$ler_resultado['Id']."</td>";*/
     }
+    echo $Lista;
 }    
-?>
-</body>
-</html>
