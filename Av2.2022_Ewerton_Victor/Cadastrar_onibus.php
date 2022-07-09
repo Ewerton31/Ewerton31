e366134aab5e948a3e19ba4dfbe6639645264a06
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-09 08:11:40 -0300

    Update Cadastrar_onibus.php

--- Av2.2022_Ewerton_Victor/Cadastrar_onibus.php
+++ Av2.2022_Ewerton_Victor/Cadastrar_onibus.php
@@ -1,6 +1,7 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     include_once("conexao.php");
+    $id = $_POST["id"];
     $marca = $_POST["marca"];
     $modelo = $_POST["modelo"];
     $qtdA = $_POST["qtdAssentos"];
@@ -8,6 +9,18 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $tar = $_POST["TArCondionado"];
     $chassi = $_POST["chassis"];
     $placa = $_POST["placa"];
-    $sql = "INSERT INTO `onibus`(`Marca`, `Modelo`, `qtdAssentos`, `temBanheiro`, `temArCondicionado`, `Chassis`, `Placa`) VALUES ('$marca','$modelo','$qtdA','$tbam','$tar','$chassi','$placa');";
+    if($tbam=='NAO'){
+        $tbam=0;
+    }
+    else{
+        $tbam=1;
+    }
+    if($tar=='NAO'){
+        $tar=0;
+    }
+    else{
+        $tar=1;
+    }
+    $sql = "INSERT INTO `onibus`(`id`, `marca`, `modelo`, `qtdAssentos`, `temBanheiro`, `temArCondicionado`, `chassis`, `placa`) VALUES ('$id','$marca','$modelo','$qtdA','$tbam','$tar','$chassi','$placa');";
     $resultado = mysqli_query($conn, $sql);
 }    
