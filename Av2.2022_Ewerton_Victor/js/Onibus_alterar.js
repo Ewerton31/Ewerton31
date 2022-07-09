6a37909189aa963cfff585794f780b3121addaa2
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-09 08:19:59 -0300

    Update Onibus_alterar.js

--- Av2.2022_Ewerton_Victor/js/Onibus_alterar.js
+++ Av2.2022_Ewerton_Victor/js/Onibus_alterar.js
@@ -17,8 +17,6 @@ AltOnibus.addEventListener("submit", async (e)=>{
         });
         const respAlt = await dadosAlt.text();
         TelaAlt.innerHTML = respAlt;
-        /*"Id:<input type= text name= 'id' value='' readonly ><br> Marca:<input type= text name= 'marca' value=''><br> Modelo:<input type= text name= 'modelo' value=''><br> Quatidade de Assentos:<input type= text name= 'qtdAssentos' value=''><br> Tem Banheiro:<input type= text name= 'TBanheiros' value=''><br> Tem Ar Condicionado:<input type= text name= 'TArCondionado' value=''><br> Chassis:<input type= text name= 'chassis' value=''><br> Placa:<input type= text name= 'placa' value=''><br><input type='submit' value='alterar'>";
-        */
     }
 });
 TelaAlt.addEventListener("submit", async (e)=>{
@@ -27,17 +25,23 @@ TelaAlt.addEventListener("submit", async (e)=>{
     if(document.getElementById("marca").value ==="" || document.getElementById("modelo").value ==="" || document.getElementById("qtdAssentos").value ==="" || document.getElementById("TBanheiros").value ==="" || document.getElementById("TArCondionado").value ==="" || document.getElementById("chassis").value ===""  || document.getElementById("placa").value ===""){
         e.preventDefault();
         console.log("Erro: Necessario Preencher todos os campo nome");
-        ErroCadastro.innerHTML = "Erro: Necessario Preencher todos os campo nome pra alterar";
+        ErroCadastro.innerHTML = "Erro: Necessario Preencher todos os campos";
     }else{
-        e.preventDefault();
-        ErroCadastro.innerHTML = "Alterado Com Sussesso";
-        const Alt = new FormData(TelaAlt);
-        Alt.append("add", 1);
-        const dadosAlt2 = await fetch("./Alterar_Onibus.php",{
-            method:"POST",
-            body: Alt,
-        });
-        const respAlt2 = await dadosAlt2.text();
-        TelaAlt.innerHTML = respAlt2;
+        if((document.getElementById("TBanheiros").value == 'SIM' || document.getElementById("TBanheiros").value == 'NAO') && (document.getElementById("TArCondionado").value == 'SIM' || document.getElementById("TArCondionado").value =='NAO')){    
+            e.preventDefault()
+            ErroCadastro.innerHTML = "Alterado Com Sussesso";
+            const Alt = new FormData(TelaAlt);
+            Alt.append("add", 1);
+            const dadosAlt2 = await fetch("./Alterar_Onibus.php",{
+                method:"POST",
+                body: Alt,
+            });
+            const respAlt2 = await dadosAlt2.text();
+            TelaAlt.innerHTML = respAlt2;
+        }else{
+            e.preventDefault()
+            console.log("ENTROU");
+            ErroCadastro.innerHTML = "Digite SIM ou NAO Maiusculo";
+        }
     }
 });
