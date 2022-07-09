3245574f20557e292ef63e562000b8fd644fb194
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-09 08:14:51 -0300

    Update Lista_todos_Onibus.php

--- Av2.2022_Ewerton_Victor/Lista_todos_Onibus.php
+++ Av2.2022_Ewerton_Victor/Lista_todos_Onibus.php
@@ -6,7 +6,19 @@ if($_SERVER["REQUEST_METHOD"]=="GET"){
     $Lista="";
     while($ler_resultado = mysqli_fetch_assoc($resultado)){
         extract($ler_resultado);
-        $Lista .= "<tr><td>$Id</td><td>$Marca</td><td>$Modelo</td><td>$qtdAssentos</td><td>$temBanheiro</td><td>$temArCondicionado</td><td>$Chassis</td><td>$Placa</td></tr>";
+        if($ler_resultado['temBanheiro']==0){
+            $res_Banheiros="NAO";
+        }
+        else{
+            $res_Banheiros="SIM";
+        }
+        if($ler_resultado['temArCondicionado']==0){
+            $res_Ar="NAO";
+        }
+        else{
+            $res_Ar="SIM";
+        }
+        $Lista .= "<tr><td>$id</td><td>$marca</td><td>$modelo</td><td>$qtdAssentos</td><td>$res_Banheiros</td><td>$res_Ar</td><td>$chassis</td><td>$placa</td></tr>";
     }
     echo $Lista;
-}    
+}  
