3c9f256be005da518a80ab776eaac40467037c80
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-13 13:39:22 -0300

    Update faeterj3dawnoite.sql

--- Av2.2022_Ewerton_Victor/faeterj3dawnoite.sql
+++ Av2.2022_Ewerton_Victor/faeterj3dawnoite.sql
@@ -37,8 +37,21 @@ CREATE TABLE IF NOT EXISTS `onibus` (
 `chassis` varchar(50) COLLATE latin1_general_ci NOT NULL,
 `placa` varchar(10) COLLATE latin1_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+--
+-- Extraindo dados da tabela `onibus`
+--
+INSERT INTO `onibus` (`id`, `marca`, `modelo`, `qtdAssentos`, `temBanheiro`, `temArCondicionado`, `chassis`, `placa`) VALUES
+('9', 'Torino ', '157', 18, 1, 1, 'LO 815', 'TP134567'),
+('2', 'Volvo', '717', 22, 0, 0, 'Monobloco', 'E200011'),
+('3', 'Jimbei', '455', 18, 0, 1, 'Scania', 'AU23476'),
+('4', 'Volkswagen', '820', 20, 1, 1, 'Subchassi', 'BR30651'),
+('5', 'Volvo', '234', 22, 1, 1, 'Scania', 'JR67790'),
+('6', 'GranVia', '854', 20, 1, 1, 'Subchassi', 'URI2022'),
+('7', 'Volvo', '790', 24, 1, 0, 'Scania', 'WS234512'),
+('1', 'Volkswagen', '549', 18, 1, 0, 'Monobloco', 'URI2019'),
+('8', 'Volvo', '717', 27, 1, 1, 'Subchassi', 'TV20043'),
+('10', 'Jimbei', '718', 26, 0, 0, 'LO 815', 'PO20034');
 COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
