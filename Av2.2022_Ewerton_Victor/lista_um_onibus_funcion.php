614ee373d2bc52c2cdfc2d9716753d9acbe039cd
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-14 01:53:42 -0300

    Update lista_um_onibus_funcion.php

--- Av2.2022_Ewerton_Victor/lista_um_onibus_funcion.php
+++ Av2.2022_Ewerton_Victor/lista_um_onibus_funcion.php
@@ -7,21 +7,23 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $Lista="";
     if($resultado->num_rows>0){
         $ler_resultado = mysqli_fetch_assoc($resultado);
-        if($ler_resultado['temBanheiro']==0){
+        $jsom = json_encode($ler_resultado);
+        $obj = json_decode($jsom);
+        if($obj->temBanheiro==0){
             $res_Banheiros="NAO";
         }
         else{
             $res_Banheiros="SIM";
         }
-        if($ler_resultado['temArCondicionado']==0){
+        if($obj->temArCondicionado==0){
             $res_Ar="NAO";
         }
         else{
             $res_Ar="SIM";
         }
         $Lista.="<table border='1'><thead><tr><th>Id</th><th>Marca</th><th>Modelo</th><th>qtdAssentos</th><th>temBanheiro</th><th>temArCondicionado</th><th>Chassis</th><th>Placa</th></tr></thead>";
-        extract($ler_resultado);
-        $Lista.= "<tr><td>$id</td><td>$marca</td><td>$modelo</td><td>$qtdAssentos</td><td>$res_Banheiros</td><td>$res_Ar</td><td>$chassis</td><td>$placa</td></tr></table>";        
+
+        $Lista.= "<tr><td>$obj->id</td><td>$obj->marca</td><td>$obj->modelo</td><td>$obj->qtdAssentos</td><td>$res_Banheiros</td><td>$res_Ar</td><td>$obj->chassis</td><td>$obj->placa</td></tr></table>";        
     }
     else{
         $Lista.= "Id Nao encontrada";
