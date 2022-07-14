3b43f18acb821f003e392c8219115a58e1fa8ced
Ewerton31 <91227349+Ewerton31@users.noreply.github.com>
2022-07-14 01:55:00 -0300

    Update pesquisar_onibus.php

--- Av2.2022_Ewerton_Victor/pesquisar_onibus.php
+++ Av2.2022_Ewerton_Victor/pesquisar_onibus.php
@@ -7,24 +7,24 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $Valore="";
     if($resultado->num_rows>0){
         $ler_resultado = mysqli_fetch_assoc($resultado);
-        extract($ler_resultado);
-        if($ler_resultado['temBanheiro']==0){
+        $jsom = json_encode($ler_resultado);
+        $obj = json_decode($jsom);
+        if($obj->temBanheiro==0){
             $res_Banheiros="NAO";
         }
         else{
             $res_Banheiros="SIM";
         }
-        if($ler_resultado['temArCondicionado']==0){
+        if($obj->temArCondicionado==0){
             $res_Ar="NAO";
         }
         else{
             $res_Ar="SIM";
         }
-        $Valore .= "Id:<input type= number name= 'id' value= $id id= 'id' readonly><br> Marca:<input type= text name= 'marca' value=$marca id= 'marca'><br> Modelo:<input type= text name= 'modelo' value=$modelo id= 'modelo'><br> Quatidade de Assentos:<input type= number name= 'qtdAssentos' value=$qtdAssentos id= 'qtdAssentos'><br> Tem Banheiro:<input type= text name= 'TBanheiros' value=$res_Banheiros id= 'TBanheiros'><br> Tem Ar Condicionado:<input type= text name= TArCondionado value=$res_Ar id= 'TArCondionado'><br> Chassis:<input type= text name= 'chassis' value=$chassis id= 'chassis'><br> Placa:<input type= text name= 'placa' value=$placa id= 'placa'><br><input type='submit'value='alterar'>";
+        $Valore .= "Id:<input type= number name= 'id' value= $obj->id id= 'id' readonly><br> Marca:<input type= text name= 'marca' value=$obj->marca id= 'marca'><br> Modelo:<input type= text name= 'modelo' value=$obj->modelo id= 'modelo'><br> Quatidade de Assentos:<input type= number name= 'qtdAssentos' value=$obj->qtdAssentos id= 'qtdAssentos'><br> Tem Banheiro:<input type= text name= 'TBanheiros' value=$res_Banheiros id= 'TBanheiros'><br> Tem Ar Condicionado:<input type= text name= TArCondionado value=$res_Ar id= 'TArCondionado'><br> Chassis:<input type= text name= 'chassis' value=$obj->chassis id= 'chassis'><br> Placa:<input type= text name= 'placa' value=$obj->placa id= 'placa'><br><input type='submit'value='alterar'>";
     }
     else{
         $Valore .= "Id nao encontrado";
     }
     echo $Valore;
 }    
-    
